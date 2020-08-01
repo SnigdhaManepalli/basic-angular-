6ca16a62f4be5dfefca7ffc17d8aabd0755ea2bb
snigdha manepalli <snigdha.mrm@gmail.com>
2020-08-01 16:39:34 +0530

    UPDATE: using angular material worked with radio buttons,selects ,inputs,checkboxes.

--- secondp/src/app/app.module.ts
+++ secondp/src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import {  FormsModule } from '@angular/forms'
 
+
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses/courses.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 
 @NgModule({
@@ -14,10 +22,16 @@ import { ContactFormComponent } from './contact-form/contact-form.component';
     CoursesComponent,
     ContactFormComponent
   ],
-  imports: [
+  imports: [ 
     BrowserModule,
     AppRoutingModule,
-    FormsModule 
+    FormsModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- secondp/src/app/courses/courses.component.ts
+++ secondp/src/app/courses/courses.component.ts
@@ -5,11 +5,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.css']
 })
-export class CoursesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class CoursesComponent {
+  isChecked=true;
+  onChange($event) {
+    console.log($event);
   }
-
+  colors = [
+    {id:1 , name: 'Red'},
+    {id:2 , name: 'Green'},
+    {id:3 , name: 'Blue'},
+  ]
+  color=2;
 }
